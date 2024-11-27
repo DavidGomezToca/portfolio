ddef39f4084efacbbd92ee179c87da87d95b7cfa
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-11-27 15:15:50 +0100

    PORTFOLIO 0.12.0
    Add Back To Top Button
    Update Footer.js
    Update index.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,30 @@
-import { useContext } from 'react'
-import { ThemeContext } from '../contexts/ThemeContext'
-import { TranslationsContext } from '../contexts/TranslationsContext'
+import { useContext, useState, useEffect } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { TranslationsContext } from "../contexts/TranslationsContext";
 
-import SocialMedia from "./SocialMedia"
+import SocialMedia from "./SocialMedia";
 
 export default function Footer() {
-    const { theme } = useContext(ThemeContext)
-    const { translations } = useContext(TranslationsContext)
+    const { theme } = useContext(ThemeContext);
+    const { translations } = useContext(TranslationsContext);
 
-    const sections = translations.sections
-    const version = require("../../package.json").version
+    const sections = translations.sections;
+    const version = require("../../package.json").version;
+
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            setIsScrolled(scrollPosition > 200);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
         <footer className={`footer color--${theme}--1 background--${theme}--2`}>
@@ -21,6 +36,11 @@ export default function Footer() {
                 <span>PORTFOLIO v{version}</span>
                 <span>COPYRYGT 2024</span>
             </div>
+            <a
+                className={`back-to-top color--${theme}--2 color--${theme}--1--hover background--${theme}--2--hover ${isScrolled ? "visible" : "hidden"}`}
+                href={`#${sections[0]}`}>
+                <i className="fa-solid fa-angles-up" />
+            </a>
         </footer>
-    )
+    );
 }
