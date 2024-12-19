c46d9b1cc576c523e9aaaa00f12af92aeb0e2326
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-12-19 16:27:19 +0100

    PORTFOLIO 0.17.0
    Update About Me Section Responsive
    Update Curriculum Section Responsive
    Update index.css
    Update SkillsSection.js

--- src/components/sections/SkillsSection.js
+++ src/components/sections/SkillsSection.js
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay, EffectCoverflow } from 'swiper/modules'
 import { ThemeContext } from '../../contexts/ThemeContext'
 import { TranslationsContext } from '../../contexts/TranslationsContext'
+import { useWindowSize } from '../../contexts/WindowSizeContext'
 
 import "swiper/css"
 import "swiper/css/effect-coverflow"
@@ -11,35 +12,39 @@ import "swiper/css/navigation"
 
 import SkillsData from "../../data/skillsData.json"
 
+// TODO: Add Responsive
 export default function SkillsSection() {
     const { theme } = useContext(ThemeContext)
     const { translations } = useContext(TranslationsContext)
+    const isBelow1100px = useWindowSize()
 
     const sections = translations.sections
     const skillsTexts = translations.skills
     const skills = SkillsData
 
     return (
-        <section id={sections[2]} className={`section__skills color--${theme}--1 background--${theme}--2`}>
-            <h1 className="section__title">{skillsTexts[0]}</h1>
-            <div className="carousel__container">
-                <Swiper
-                    effect={"coverflow"}
-                    autoplay={{ delay: 1000, disableOnInteraction: false }}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    loop={true}
-                    slidesPerView={"3"}
-                    modules={[Autoplay, EffectCoverflow]}
-                    className="swiper__container">
-                    {skills.map((skill) => (
-                        <SwiperSlide key={skill.file} className="swiper__slide" >
-                            <SkillSlide skill={skill} />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div >
-        </section>
+        !isBelow1100px && (
+            <section id={sections[2]} className={`section__skills color--${theme}--1 background--${theme}--2`}>
+                <h1 className="section__title">{skillsTexts[0]}</h1>
+                <div className="carousel__container">
+                    <Swiper
+                        effect={"coverflow"}
+                        autoplay={{ delay: 1000, disableOnInteraction: false }}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        loop={true}
+                        slidesPerView={"3"}
+                        modules={[Autoplay, EffectCoverflow]}
+                        className="swiper__container">
+                        {skills.map((skill) => (
+                            <SwiperSlide key={skill.file} className="swiper__slide" >
+                                <SkillSlide skill={skill} />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div >
+            </section>
+        )
     )
 
     function SkillSlide({ skill }) {
